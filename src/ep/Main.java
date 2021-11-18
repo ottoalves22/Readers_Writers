a6dd8d4f61343afc27f9f7639831b245cd1c81ee
ottoalves22 <ottoalves22@gmail.com>
2021-11-17 21:36:33 -0300

    Ah puta uqe pariu vai tomar no cu foda-se

--- src/ep/Main.java
+++ src/ep/Main.java
@@ -3,37 +3,45 @@ package ep;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Random;
+import ep.ReaderWriter.Writer;
+import ep.ReaderWriter.Reader;
 
 public class Main {
-    public Thread[] threads;
+    static Thread[] threads;
 
 
 
     public static void main(String[] args) throws IOException, FileNotFoundException, InterruptedException  {
         BD leitor_txt = new BD();
         leitor_txt.lerBd();
-        /*
-        for (int i=0; i<100; i++){
-            System.out.println(leitor_txt.texto[i]);
-        }*/
+        int proporcao = 69;
+        createThreads(proporcao);
+        for(Thread thread : threads){
+            System.out.print("gang");
+            thread.start();
+            //System.out.println(leitor_txt.texto[i]);
+        }
 
     }
 
 
-
-    public void createThreads(int proportion, int implementation) throws FileNotFoundException {
-        this.threads = new Thread[100];
+    public static void createThreads(int proportion) throws FileNotFoundException {
+        threads = new Thread[100];
         Random rand = new Random();
         int aux_rand;
         for(int i=0; i<proportion; i++){
-            aux_rand = rand.nextInt(101);
-            if(this.threads[aux_rand] == null){
-                this.threads[aux_rand] = new Thread(new Writer());
+            aux_rand = rand.nextInt(100);
+            if(threads[aux_rand] == null){
+                threads[aux_rand] = new Thread(new Writer());
+                //System.out.print("Escreveu o Writer");
+                //System.out.println(aux_rand);
             }
         }
-        for(int i=0; i<=100; i++){ // preenche o coiso com os threads pra cada classe
-            if(this.threads[i] == null){
-                this.threads[i] = new Thread(new Reader());
+        for(int i=0; i<100; i++){ // preenche o coiso com os threads pra cada classe
+            if(threads[i] == null){
+                threads[i] = new Thread(new Reader());
+                //System.out.print("Escreveu o Reader ");
+                //System.out.println(i);
             }
         }
     }
@@ -41,8 +49,8 @@ public class Main {
     //AQUI DA PRA MEXER MAIS, TA MUITO PARECIDO
     public void joinThreads() throws InterruptedException{
         int i = 0;
-        while(i<this.threads.length){
-            this.threads[i].join();
+        while(i<threads.length){
+            threads[i].join();
             i++;
         }
     }
---
+++ src/ep/ReaderWriter.java
@@ -0,0 +1,57 @@
+package ep;
+
+import java.util.concurrent.Semaphore;
+
+class ReaderWriter{
+    static int readCount = 0; //USAR VOLATILE SE NECESSARIO https://codereview.stackexchange.com/questions/127234/reader-writers-problem-using-semaphores-in-java
+    static Semaphore readLock = new Semaphore(1);
+    static Semaphore writeLock = new Semaphore(1);
+
+    static class Reader implements Runnable {
+        @Override
+        public void run() {
+            try {
+                //Acquire Section
+                readLock.acquire();
+                readCount++;
+                if (readCount == 1) {
+                    writeLock.acquire();
+                }
+                readLock.release();
+
+                //Reading section
+                System.out.println("Thread "+Thread.currentThread().getName() + " is READING");
+                Thread.sleep(1500);
+                System.out.println("Thread "+Thread.currentThread().getName() + " has FINISHED READING");
+
+                //Releasing section
+                readLock.acquire();
+                readCount--;
+                if(readCount == 0) {
+                    writeLock.release();
+                }
+                readLock.release();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    static class Writer implements Runnable {
+        @Override
+        public void run() {
+            try {
+                writeLock.acquire();
+                System.out.println("Thread "+Thread.currentThread().getName() + " is WRITING");
+                Thread.sleep(2500);
+                System.out.println("Thread "+Thread.currentThread().getName() + " has finished WRITING");
+                writeLock.release();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+}
+
+// VEIO DAQUI https://codereview.stackexchange.com/questions/127234/reader-writers-problem-using-semaphores-in-java
